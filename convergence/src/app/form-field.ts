74c85389292c1ddc17ca1e6c37ddfbbf989ac858
jdaza13 <jdaza1327@gmail.com>
2018-04-02 21:08:32 +0000

    base for first component

---
+++ convergence/src/app/form-field.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+/** @title Simple form field */
+@Component({
+  selector: 'form-field',
+  templateUrl: './form-field.html',
+  styleUrls: ['./form-field.css']
+})
+export class FormFieldComponent {}
